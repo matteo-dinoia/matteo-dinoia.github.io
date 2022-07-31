61b0d7690160e7856404930b43a9c673bf62e243
Matteo Di Noia <matteodinoia02@gmail.com>
2022-07-31 19:03:35 +0200

    test http request on github

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
+//ON LOAD
 document.addEventListener("DOMContentLoaded", function(){
     loadHome();
 });
 
-
+//CONSTANT
+//var basePath="";
+var basePath="https://github.com/matteo-dinoia/matteo-dinoia";
 
 //HOME PAGE LOADING
 function loadHome(){
