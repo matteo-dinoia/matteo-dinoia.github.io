3e5c708e3c213c9ab0c1f16cdf5317061ef7cd26
Matteo Di Noia <matteodinoia02@gmail.com>
2022-07-31 19:40:29 +0200

    automation of const value

--- js/script.js
+++ js/script.js
@@ -3,13 +3,16 @@ document.addEventListener("DOMContentLoaded", function(){
     loadHome();
 });
 
-//CONSTANT
-//var basePath="";
-var basePath="https://raw.githubusercontent.com/matteo-dinoia/matteo-dinoia.github.io/master/js/";
+//CONSTANT FOR GETTING FILE
+var basePath="";
+if(window.location.hostname ==="matteo-dinoia.github.io"){
+    basePath="https://raw.githubusercontent.com/matteo-dinoia/matteo-dinoia.github.io/master/js/";
+}
+    
 
 //HOME PAGE LOADING
 function loadHome(){
-    utilsHttp.request(basePath+"../snippets/", function(response){
+    utilsHttp.request(basePath+"../snippets/home-snippet.html", function(response){
         console.log(response);
         utilsHtml.insertInnerHtml("#main-content", response);
     });
