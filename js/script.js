05a60a1d5bed3e93db89979c52cd5caa1ac5712f
Matteo Di Noia <matteodinoia02@gmail.com>
2022-07-31 19:05:38 +0200

    fix last commit

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ document.addEventListener("DOMContentLoaded", function(){
 
 //CONSTANT
 //var basePath="";
-var basePath="https://github.com/matteo-dinoia/matteo-dinoia";
+var basePath="https://github.com/matteo-dinoia/matteo-dinoia/js/";
 
 //HOME PAGE LOADING
 function loadHome(){
-    utilsHttp.request("../snippets/", function(response){
+    utilsHttp.request(basePath+"../snippets/", function(response){
         console.log(response);
         utilsHtml.insertInnerHtml("#main-content", response);
     });
